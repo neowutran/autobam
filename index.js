6132a0620482c82fb58e0e5f71c3072ba890ba56
Yukikoo <me@neowutran.ovh>
2017-03-26 12:38:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -33,26 +33,26 @@ module.exports = function AutoBam(dispatch){
 		console.info("bam start try quest");
 		var intervalTime = Math.floor((Math.random() * 10) + 1) + 60;
 		intervalId = setInterval(() => { 
-			 dispatch.toServer('cRequestGuildInfo', {
+			 dispatch.toServer('C_REQUEST_GUILD_INFO',1, {
                  guildId: myGuildId, 
 				 type: 6
 			 });	
 		}, intervalTime); 
     }
 	
-	dispatch.hook('sGuildInfo', (event) => {
+	dispatch.hook('S_GUILD_INFO',1, (event) => {
 		myGuildId = event.id;
 		return true;
 	});
 	
-	dispatch.hook('sGuildQuestList', (event) => {
+	dispatch.hook('S_GUILD_QUEST_LIST',1, (event) => {
 		if(enabled === false){return true;}
 		for(var i = 0; i < event.quests.length ; i++){
 			for(var j = 0; j < event.quests[i].targets.length; j++){
 				if(parseInt(event.quests[i].targets[j].total) === 1){
 					console.info("bam quest found, sending accept");
 					setEnable(false);
-					dispatch.toServer('cRequestStartGuildQuest', {
+					dispatch.toServer('C_REQUEST_START_GUILD_QUEST',1, {
 						questId: event.quests[i].id
 					});
 					return false;
